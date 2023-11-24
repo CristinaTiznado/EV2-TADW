0c14965b6d3f07d742de6980da4d23871f6f85b3
Unachascaloca <bryanruizc@hotmail.com>
2023-11-24 01:27:16 -0300

    rutas.

--- Tinder Backend/routes/api.php
+++ Tinder Backend/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\PerroController;
+use App\http\Controllers\InteraccionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/api/perros/random', [PerroController::class, 'GetRandom']);
+Route::get('/api/perros/candidatos/{id}', [PerroController::class, 'getCandidatos']);
+Route::get('/api/perros/aceptados/{id}', [PerroController::class, 'getAceptados']);
+Route::get('/api/perros/rechazados/{id}', [PerroController::class, 'getRechazados']);
+
+Route::post('/api/interacciones/preferencia', [InteraccionController::class, 'preferencia']);
+
+Route::resource('perros',PerroController::class);
+Route::resource('interacciones',InteraccionController::class);
